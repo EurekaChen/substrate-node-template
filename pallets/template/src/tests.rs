527a6d18315de6cf886cb51944c35e88beafdfc5
Eureka Chen <eureka@ee-studio.com>
2021-01-07 19:49:19 +0800

    Update tests.rs
    作业

--- pallets/template/src/tests.rs
+++ pallets/template/src/tests.rs
@@ -1,23 +1,48 @@
 use crate::{Error, mock::*};
 use frame_support::{assert_ok, assert_noop};
+use super::*;
 
 #[test]
-fn it_works_for_default_value() {
-	new_test_ext().execute_with(|| {
-		// Dispatch a signed extrinsic.
-		assert_ok!(TemplateModule::do_something(Origin::signed(1), 42));
-		// Read pallet storage and assert an expected result.
-		assert_eq!(TemplateModule::something(), Some(42));
-	});
+fn create_claim_works(){
+	new_test_ext().execute_with(||{
+		let claim=vec![0,1];
+		assert_ok!(TemplateModule::create_claim(Origin::signed(1),claim.clone()));
+		assert_eq!(Proofs::<Test>::get(&claim),(1,frame_system::Module::<Test>::block_number()));
+	})
+}
+
+
+#[test]
+fn create_claim_failed_when_claim_already_exist(){
+	new_test_ext().execute_with(||{
+		let claim=vec![0,1];
+		let _=TemplateModule::create_claim(Origin::signed(1),claim.clone());
+
+		assert_noop!(
+			TemplateModule::create_claim(Origin::signed(1),claim.clone()),
+			Error::<Test>::ProofAlreadyExist
+		);
+	})
+}
+
+
+#[test]
+fn revoke_claim_works(){
+	new_test_ext().execute_with(||{
+		let claim=vec![0,1];
+		let _=TemplateModule::create_claim(Origin::signed(1),claim.clone());
+		assert_ok!(TemplateModule::revoke_claim(Origin::signed(1),claim.clone()));
+	})
 }
 
 #[test]
-fn correct_error_for_none_value() {
-	new_test_ext().execute_with(|| {
-		// Ensure the expected error is thrown when no value is present.
+fn revoke_claim_failed_when_claim_is_not_exist(){
+	new_test_ext().execute_with(||{
+		let claim=vec![0,1];
+
 		assert_noop!(
-			TemplateModule::cause_error(Origin::signed(1)),
-			Error::<Test>::NoneValue
+			TemplateModule::revoke_claim(Origin::signed(1),claim.clone()),
+			Error::<Test>::ClaimNotExist
 		);
-	});
+	})
 }
